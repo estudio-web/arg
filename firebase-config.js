a987b961ade8f74d6579e0c26c047e713d901b1e
estudio-web <tompsonfulner@gmail.com>
2026-04-23 19:01:31 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -14,7 +14,7 @@ export const APP_CONFIG = {
   currencySymbol: "$",
   whatsappNumber: "5490000000000",
   appName: "IN-STORE",
-  paymentLink: "https://estudio-web.github.io/arg/payments/payments.html?u=c2Q2sYbqeYQc5GFHU1AodfJQ8O12&p=Z7sClHy1we4MK4sIINBQ",
+  paymentLink: "https://estudio-web.github.io/arg/payments/payments.html?u=c2Q2sYbqeYQc5GFHU1AodfJQ8O12&p=0YVmAmusZtfiMtsItBf2",
 };
 
 // Planes disponibles del SaaS
